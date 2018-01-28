f9d88d07baf1b5eab699bcfcaf5c1b9b6d1fdf66
Jussi Ristimäki <rutle4@gmail.com>
2018-01-28 23:12:20 +0200

    Setting up database and basic functionality for homepage.

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
 'use strict';
+var mongoose        = require('mongoose');
+var SwaggerExpress  = require('swagger-express-mw');
+var express         = require('express');
+//const db            = require('./db/database.js')
+var port            = process.env.PORT || 10010;
+var passport        = require('passport')
+var session         = require('express-session')
+const exphbs        = require('express-handlebars');
+const path          = require('path');
+const publicPath    = path.join(__dirname, '/views');
 
-var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
+const app = express();
+
+app.engine('.hbs', exphbs({
+	defaultLayout: 'main',
+	extname: '.hbs',
+	layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+
+app.set('view engine', '.hbs');
+app.use('/', express.static(publicPath));
+
+app.use(session({
+	secret: "It's a secret!",
+	resave: false,
+	saveUninitialized: true,
+	cookie: { secure: false}
+	})
+);
+
 module.exports = app; // for testing
 
 var config = {
   appRoot: __dirname // required config
 };
-
+app.get('/', (req, res) => { // '/' url it is listening
+	res.render('home');
+});
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
   // install middleware
   swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
+
   app.listen(port);
 
   if (swaggerExpress.runner.swagger.paths['/hello']) {
