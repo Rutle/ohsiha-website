4f26c8dfbc03aa4e5dc769f2b36600ae35b9688b
Jussi Ristimäki <rutle4@gmail.com>
2018-01-29 13:17:55 +0200

    Cleaning up routing. Added alert message to login screen.

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ var SwaggerExpress  = require('swagger-express-mw');
 var swaggerUi       = require('swagger-ui-express');
 const YAML          = require('yamljs');
 var express         = require('express');
-var port            = process.env.PORT || 10010;
+var port            = process.env.PORT || 5000;
 var passport        = require('passport')
 var flash           = require('connect-flash');
+var morgan          = require('morgan');
 var cookieParser    = require('cookie-parser');
 var bodyParser      = require('body-parser');
 var session         = require('express-session')
@@ -16,6 +17,9 @@ const exphbs        = require('express-handlebars');
 const path          = require('path');
 const publicPath    = path.join(__dirname, '/views');
 const swaggerDocument = YAML.load('./api/swagger/swagger.yaml');
+
+// Routes
+var routes = require('./app/routers');
 
 // Connection to database.
 //mongoose.connect('mongodb://localhost/test');
@@ -31,6 +35,7 @@ app.engine('.hbs', exphbs({
 
 app.set('view engine', '.hbs');
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/'));
+app.use('/popper', express.static(__dirname + '/node_modules/popper.js/'));
 app.use('/', express.static(publicPath));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -44,40 +49,41 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
+app.use(morgan('dev')); // log every request to the console
 
 // Routing of URLs
-
+// Base URL
 app.get('/', (req, res) => { // '/' url it is listening
 	res.render('home');
 });
 
-app.get('/login', function(req, res) {
-    res.render('login');
-});
+app.get('/profile', isLoggedIn, routes.userProfile);
+app.get('/logout', routes.userLogout);
+app.get('/login', routes.userLogin);
+app.get('/signup', routes.userSignup);
 
-// process the login form
 app.post('/login', passport.authenticate('local-login', {
     successRedirect : '/profile', // redirect to the secure profile section
     failureRedirect : '/login', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
-}));
+	})
+);
 
-// SIGNUP =================================
-// show the signup form
-app.get('/signup', function(req, res) {
-    res.render('signup', {
-      message: req.flash('signupMessage')
-    });
-});
-
-// process the signup form
+// process the signup form (POST)
 app.post('/signup', passport.authenticate('local-signup', {
     successRedirect : '/profile', // redirect to the secure profile section
     failureRedirect : '/signup', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
 }));
 
-//module.exports = app; // for testing
+// Check if user is logged in with a middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/');
+}
+
+module.exports = app; // for testing
 
 var config = {
   appRoot: __dirname // required config
@@ -91,10 +97,10 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   swaggerExpress.register(app);
 
   app.listen(port);
-
+	/*
   if (swaggerExpress.runner.swagger.paths['/hello']) {
     console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+  }*/
 });
 
 // Include the API documentation using Swagger UI
