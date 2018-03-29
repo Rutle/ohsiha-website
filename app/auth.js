70159933df2069fb0094fcd1257eb12b9194bd66
Jussi Ristimäki <rutle4@gmail.com>
2018-03-29 17:36:27 +0300

    Revert "Removed auth.js tracking due to containing api keys."
    This reverts commit 667d6f54f9f73a41e3db7bffdf14becc0cdbd48f.

--- app/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'twitterAuth' : {
-        'consumerKey'       : 'WQhjzRI7dHOdx1WyAWRRZu11p',
-        'consumerSecret'    : 'h2li9AJtFiDFYejo1Srl3t1RD7b64LB1wJ54EFjDn0pC3wugXL',
-        'callbackURL'       : 'http://localhost:5000/auth/twitter/callback'
-    }
-};
