a2176ed3b67c8b89f9348b7325cbbc49d12a4946
Jussi Ristimäki <rutle4@gmail.com>
2018-04-01 14:52:37 +0300

    Added schema for holding fetched twitter data.

---
+++ app/models/twitterdata.js
@@ -0,0 +1,26 @@
+// app/models/twitterdata.js
+// Javascript file for MongoDB twitterdata-schema intended for storing twitter data.
+
+var mongoose = require('mongoose');
+
+// MongoDB schema for an twitter data model.
+var twitterDataSchema = mongoose.Schema({
+  author: {                                 // ObjectId of the user
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  content: [{                               // Array of tweets.
+    tweet: String
+  ]},
+  dateCreated: {                            // Creation date of the document.
+    type: Date,
+    default: Date.now
+  },
+  dateModified: {                           // Modification date of the document.
+    type: Date,
+    default: Date.now
+  },
+});
+
+module.exports = mongoose.model('TweetData', twitterDataSchema);
