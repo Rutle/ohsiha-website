a0b2f09b31abc7aeee794a81214e44303a8d8b69
Jussi Ristimäki <rutle4@gmail.com>
2018-04-01 22:43:50 +0300

    Cleaned up custom scripts javascript file.

--- views/js/page.js
+++ views/js/page.js
@@ -1,118 +1,33 @@
-
+// Custom scripts for the website
 
 $(function () {
   $('#fetchData').on('submit', function (e) {
     console.log("pressed submit");
     e.preventDefault();
+	
+	// Disable the button for the duration of data fetching.
     $('#fetchDataSubmit').prop("disabled", true);
     $('#fetchDataSubmit').val('Please wait');
+	
+	// Ajax post call to make server side fetch the tweet data for current user.
     $.ajax({
       type: 'POST',
       url: 'http://localhost:5000/dashboard',
       data: {form: 'fetchData'},
       dataType: 'json',
       success: function (data) {
+		// Make submit button pressable and change the text back.
         $('#fetchDataSubmit').prop("disabled", false);
         $('#fetchDataSubmit').val('Update/Fetch twitter data');
-        alert(JSON.stringify(data));
+		
+		// Remove focus on the submit button.
         $('#fetchDataSubmit').trigger('blur');
       },
       error: function(jqXHR, textStatus, err) {
-      //show error message
+      // Perhaps send error message from server side and load it into the warning box.
       alert('text status '+textStatus+', err '+err)
       }
-  });
-  });
-
-});
-
-/*
-$(document).ready(function() {
-  $('#home').click(function (e) {
-    e.stopPropagation();
-    $('#projects').removeClass('active');
-    $('#contact').removeClass('active');
-    $(this).addClass('active');
-  });
-});
-*/
-/*
-$(document).ready(function() {
-  $('#navbar nav a').click(function (event) {
-    $('#navbar nav a').removeClass('active');
-    $(this).addClass('active');
-    console.log($(this));
-
-  });
-});
-*/
-/*
-$(document).ready(function() {
-$("#navbar ul li a").click(function () {
-    $("#navbar ul li a").removeClass("active");
-    $(this).addClass("active");
-	console.log(this)
-  });
-});
-*/
-/*
-$(document).ready(function() {
-$("#navbar a").click(function () {
-    $("#navbar a").removeClass("active");
-    $(this).addClass("active");
-  });
-});
-*/
-/*
-$(function() {
-  if (location.pathname.split("/")[1] === "") {
-    $('#home').addClass('active');
-  } else {
-    $('#navbarheader a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
-  }
-
-});
-*/
-/*
-$(document).ready(function() {
-  $('#hiraganaBtn').click(function () {
-
-
-  });
-});
-*/
-
-/*
-$(document).ready(function () {
-        $('nav.nav > a').click(function (e) {
-            //e.preventDefault();
-            $('nav.nav > a').removeClass('active');
-            $(this).addClass('active');
-        });
     });
-*/
+  });
 
-/*
-$(function() {
-    if ((location.pathname.split("/")[1]) !== ""){
-
-        $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
-    } else {
-		$('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
-	}
 });
-*/
-/*
-$('nav').on('click', 'a', function() {
-    $('.nav-list li.active').removeClass('active');
-    $(this).addClass('active');
-});
-*/
-/*
-$(document).ready(function() {
-	// get current URL path and assign 'active' class
-	var pathname = window.location.pathname;
-	$('#navbar nav a[href="'+pathname+'"]').parent().addClass('active');
-})
-
-*/
